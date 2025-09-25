250e4c6713deecc95cca4084dfa1add866295af5
Thigulla Chatrapathi Reddy <chatrapathireddythigulla@gmail.com>
2025-09-25 19:15:46 +0530

    Update settings.py

--- MyFirstDjango/settings.py
+++ MyFirstDjango/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "django-insecure-3i75w$c)qa@^ffu#5mu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "MyFirstDjango-19.onrender.com,127.0.0.1,localhost").split(",")
-
+ALLOWED_HOSTS = ['MyFirstDjango-19.onrender.com']
 #gunicorn == 22.0.0
 # Application definition
 

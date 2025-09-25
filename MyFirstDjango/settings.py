e2068d94581ad36aee10ee4be597387bee61c479
Thigulla Chatrapathi Reddy <chatrapathireddythigulla@gmail.com>
2025-09-25 19:05:56 +0530

    Update settings.py

--- MyFirstDjango/settings.py
+++ MyFirstDjango/settings.py
@@ -80,10 +80,10 @@ WSGI_APPLICATION = 'MyFirstDjango.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,   # keep connection open longer (better for Render)
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 # Password validation
